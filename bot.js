52516ba130da2992ada7c00ba85722ee700e52d3
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-10-20 11:20:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Diamond Codes ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Venom Network`,"http://twitch.tv/S-F")
+client.user.setGame(`Sea World`,"http://twitch.tv/S-F")
 client.user.setStatus("online")
  
 });
